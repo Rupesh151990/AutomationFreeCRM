f1a37d429376712fc4af15b4b4e177276ef16d7e
hp <hp@DESKTOP-8AK31QI>
2024-12-26 22:55:07 +0530

    Commit the Extent Report Listener, but need to derive the functions

--- src/main/java/com/qa/extentReport/ExtentReportListener.java
+++ src/main/java/com/qa/extentReport/ExtentReportListener.java
@@ -1,5 +1,53 @@
 package com.qa.extentReport;
 
-public class ExtentReportListener {
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.qa.base.TestBase;
+
+public class ExtentReportListener extends TestBase implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
